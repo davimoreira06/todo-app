b1f1d918e676068ebd21d2e6633383eb78f65d9c
davimoreira06 <davimoreira291273@gmail.com>
2023-03-28 23:27:54 -0300

    apagar-tarefas-completas

--- scripts.js
+++ scripts.js
@@ -7,26 +7,34 @@ function createTask(task, status, indice = "") {
     const item = document.createElement("ul");
     item.classList.add("ul-tasks");
     item.innerHTML = `
-      <label class="label-list">
-        <li class="li-tasks">
-          <input type="checkbox" id="check-li" name="check-li" ${status} data-indice=${indice}>
-          <img id="check-img" src="./assets/icons/icon-check.svg">
-          <span class="text-task ${status === "checked" ? "checked" : ""}">${task}</span>
-          <input type="image" class="delete-img" src="./assets/icons/icon-cross.svg"/>
-        </li>
-      </label>
+        <label class="label-list">
+          <li class="li-tasks">
+            <input type="checkbox" id="check-li" name="check-li" ${status} data-indice=${indice}>
+            <img id="check-img" src="./assets/icons/icon-check.svg">
+            <span class="text-task ${status === "checked" ? "checked" : ""}">${task}</span>
+            <input type="image" class="delete-img" src="./assets/icons/icon-cross.svg"/>
+          </li>
+        </label>
     `;
     const checkbox = item.querySelector("input[type='checkbox']");
     const span = item.querySelector(".text-task");
     checkbox.addEventListener("click", () => {
-      if (checkbox.checked) {
-        span.classList.add("checked");
-      } else {
-        span.classList.remove("checked");
-      }
+        if (checkbox.checked) {
+          span.classList.add("checked");
+          updateItemsLeft(-1);
+        } else {
+          span.classList.remove("checked");
+          updateItemsLeft(1);
+        }
     });
     document.querySelector(".tasks").appendChild(item);
-  }
+}
+
+function updateItemsLeft(change) {
+    const itemsLeft = document.querySelector("#items-left");
+    const currentCount = parseInt(itemsLeft.innerText);
+    itemsLeft.innerText = currentCount + change;
+}
 
 let myFooter = null;
 
@@ -37,17 +45,11 @@ function createFooter() {
         myFooter.classList.add("footer-tasks");
         myFooter.innerHTML = `
             <div class="ft-left">
-                <p><span id="items-left">${dataBase.length}</span> items left</p>
-            </div>
-    
-            <div class="ft-mid">
-                <span id="all-span">All</span>
-                <span id="active-span">Active</span>
-                <span id="completed-span">Completed</span>
+                <p><span id="items-left">${dataBase.filter(item => !item.status).length}</span> items left</p>
             </div>
     
             <div class="ft-right">
-                <p id="clear">Clear Completed</p>
+                <button id="clear">Clear Completed</button>
             </div>
         `;
 
@@ -55,7 +57,7 @@ function createFooter() {
     }
 }
 
-const clearTaks = () => {
+const clearTask = () => {
     const tasksList = document.querySelector(".tasks");
     while(tasksList.firstChild) {
         tasksList.removeChild(tasksList.lastChild);
@@ -63,10 +65,11 @@ const clearTaks = () => {
 }
 
 const updatePage = () => {
-    clearTaks();
+    clearTask();
     const dataBase = getDataBase();
     dataBase.forEach((item, indice) => createTask(item.task, item.status, indice));
     createFooter();
+    updateItemsLeft(0);
 }
 
 const addTaskEnter = (event) => {
@@ -89,12 +92,32 @@ const removeItem = (indice) => {
     updatePage();
 }
 
+const clearCompleted = () => {
+    const dataBase = getDataBase();
+    const newDataBase = dataBase.filter(item => item.status !== "checked");
+    setDataBase(newDataBase);
+    updatePage();
+}
+
+const updateItem = (indice) => {
+    const dataBase = getDataBase();
+    dataBase[indice].status = dataBase[indice].status === '' ? 'checked' : '';
+    setDataBase(dataBase);
+    updatePage();
+}
+
 const clickItem = (event) => {
     const element = event.target;
     console.log(element.type)
     if (element.type === "image") {
         const indice = element.dataset.indice;
         removeItem(indice);
+    } else if (element.type === 'checkbox') {
+        const indice = element.dataset.indice;
+        updateItem(indice);
+    }
+    if (element.id === "clear") {
+        clearCompleted();
     }
 }
 
@@ -103,8 +126,3 @@ document.querySelector("#input-create").addEventListener("keypress", addTaskEnte
 document.querySelector(".tasks").addEventListener("click", clickItem);
 
 updatePage();
-
-
-
-
-
